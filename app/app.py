f48a35add81acfd7de2e20142de4f93b6a6e19f6
Andrew Montalenti <andrew@parsely.com>
2013-02-09 19:14:09 -0500

    add skeleton Flask project and module

---
+++ app/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from rapid import (query_articles, search_articles, insert_article)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    articles = query_articles()
+    return render_template('index.jinja2.html', 
+                           articles=articles)
+
+@app.route('/search/<query>')
+def search(query):
+    articles = search_articles(query)
+    return render_template('index.jinja2.html', 
+                           query=query, 
+                           articles=articles)
+
+@app.route('/submit/', methods=["GET", "POST"])
+def submit():
+    if request.method == "POST":
+        return do_submit()
+    else:
+        return render_template('submit.jinja2.html')
+
+def do_submit():
+    # article = Article(title=title, link=link)
+    # insert_article(article)
+    article = insert_article()
+    return render_template("inserted.jinja2.html", article=article)
+
+def run_devserver():
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    run_devserver()
---
+++ app/rapid.py
@@ -0,0 +1,8 @@
+def query_articles():
+    return []
+
+def search_articles():
+    return []
+
+def insert_article():
+    return False

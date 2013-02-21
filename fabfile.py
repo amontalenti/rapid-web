9c842e030fb2b519c251826cd6cf1d0671183e8f
Andrew Montalenti <andrew@parsely.com>
2013-02-21 08:39:59 -0500

    "server-ize" the deploy env with supervisor/nginx

--- fabfile.py
+++ fabfile.py
@@ -4,15 +4,7 @@ from fabric.contrib.project import rsync_project
 env.use_ssh_config = True
 env.hosts = ["shared@hacknode"]
 
-def unique_id():
-    def sh(cmd):
-        return local(cmd, capture=True)
-    return "{}__{}".format(sh("whoami"), sh("hostname"))
-
-@task
-def print_my_id():
-    """Print your unique identifier."""
-    puts("UNIQUE ID: " + unique_id())
+TEAM_NAME = "hacknode1"
 
 @task
 def list_home():
@@ -20,9 +12,9 @@ def list_home():
     run("ls -lha")
 
 @task
-def list_deploys():
-    """List deployment directories."""
-    run("ls deploys")
+def list_servers():
+    """List deployment servers."""
+    run("ls servers")
 
 def virtualenv_run(cmd):
     run("source rapid-env/bin/activate && {}".format(cmd))
@@ -30,21 +22,20 @@ def virtualenv_run(cmd):
 @task
 def setup_virtualenv():
     """Set up virtualenv on remote machine."""
-    with cd("deploys/" + unique_id()):
+    with cd("servers/" + TEAM_NAME):
         run("virtualenv rapid-env")
         virtualenv_run("pip install -r requirements.txt")
 
 @task
 def run_devserver():
     """Run the dev Flask server on remote machine."""
-    with cd("deploys/" + unique_id()):
+    with cd("servers/" + TEAM_NAME):
         virtualenv_run("cd app && python app.py runserver --host=0.0.0.0 --port=8000")
 
 @task
 def deploy():
     """Deploy project remotely."""
-    run("mkdir -p deploys")
-    rsync_project(remote_dir="deploys/" + unique_id(),
+    run("mkdir -p servers")
+    rsync_project(remote_dir="servers/" + TEAM_NAME,
                   local_dir="./",
                   exclude=("*.pyc", ".git", "rapid-env", "steps", "activate"))
-

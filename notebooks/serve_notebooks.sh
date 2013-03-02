cecb36327213eec5848a52582b22adb79af2cc15
Andrew Montalenti <andrew@parsely.com>
2013-03-02 15:31:36 -0500

    improve serve_notebooks script

--- notebooks/serve_notebooks.sh
+++ notebooks/serve_notebooks.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# requires an activation script in directory above
+source ../activate
+# makes a sandbox folder
 mkdir -p $1
+# copies notebooks over
 cp *.ipynb $1/
+# goes into it
 cd $1
+# runs ipython notebook on a public IP w/ specified port
 ipython notebook --ip=* --port=$1 --pprint --browser=/bin/true
